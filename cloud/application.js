85ca6c191d0ea4b9f3ab8af1e15b300a5a973a3f
Michael Nairn <michael.nairn@feedhenry.com>
2013-12-11 15:20:07 +0000

    Initial Commit

---
+++ cloud/application.js
@@ -0,0 +1,13 @@
+var webapp = require('fh-webapp');
+var express = require('express');
+$fh = require('fh-api');
+var mainjs = require('main.js');
+
+var app = express();
+app.use('/sys', webapp.sys(mainjs));
+app.use('/mbaas', webapp.mbaas);
+app.use('/cloud', webapp.cloud(mainjs));
+
+app.use(express.static(__dirname + '/public'));
+
+module.exports = app.listen(process.env.FH_PORT || process.env.VCAP_APP_PORT || 8001);
