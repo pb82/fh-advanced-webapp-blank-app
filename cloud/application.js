cfe3c2a1b6bcae2e6a374946d6aaf2172fe45c66
Jason Madigan <jason.madigan@feedhenry.com>
2014-03-13 18:15:55 +0000

    Update

--- cloud/application.js
+++ cloud/application.js
@@ -1,8 +1,16 @@
 var express = require('express');
+var corser = require("corser");
 
 var app = express();
+app.use(corser.create());
 
 app.use(express.static(__dirname + '/public'));
+
+app.options("*", function (req, res) {
+  // CORS
+  res.writeHead(204);
+  res.end();
+});
 
 // Used for App health checking
 app.get('/sys/info/ping', function(req, res, next) {
