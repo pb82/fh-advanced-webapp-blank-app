51105f08c2aefc328fdcca81a1db6b5306a243de
Jason Madigan <jason.madigan@feedhenry.com>
2014-03-13 17:57:54 +0000

    Add ping

--- cloud/application.js
+++ cloud/application.js
@@ -4,4 +4,9 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+// Used for App health checking
+app.get('/sys/info/ping', function(req, res, next) {
+  res.end('"OK"');
+});
+
 module.exports = app.listen(process.env.FH_PORT || process.env.VCAP_APP_PORT || 8001);
