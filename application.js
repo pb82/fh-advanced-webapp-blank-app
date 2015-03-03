11a136879111817cb88fca964984de35a1e9cd89
cianclarke <cian@cianclarke.com>
2015-03-03 12:16:17 -0500

    Bind to 0.0.0.0

--- application.js
+++ application.js
@@ -18,7 +18,7 @@ app.get('/sys/info/ping', function(req, res, next) {
 });
 
 var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
-var host = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var server = app.listen(port, host, function() {
   console.log("App started at: " + new Date() + " on port: " + port); 
 });
